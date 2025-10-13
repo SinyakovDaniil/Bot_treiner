c16208753177062167235289e2b92a1431678943
Daniil Sinyakov <sinyakodd@yandex.ru>
2025-10-13 17:05:49 +0300

    Смена модели

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ client = OpenAI(
     base_url='https://openrouter.ai/api/v1/' # <-- Исправлено, слэш в конце важен
 )
 
-MODEL = "deepseek/deepseek-chat-v3.1:free"
+MODEL = "qwen/qwen2.5-vl-72b-instruct:free"
 
 # --- Подключение к SQLite ---
 conn = sqlite3.connect('trainer_bot.db', check_same_thread=False)
